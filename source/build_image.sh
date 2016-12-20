8009bb0fc4f771bc7afc758d2ec1a8a622753865
Kor de Jong <k.dejong1@uu.nl>
2016-12-20 14:24:07 +0100

    Wip

--- source/build_image.sh
+++ source/build_image.sh
@@ -6,6 +6,6 @@ docker build -t test/emis_base .
 
 # A test to see whether we can create a file in a mounted directory and
 # read it again afterwards.
-docker run --env EMIS_USER_ID=$(id -u) -v$EMIS_PROPERTY_DATA:/data test/emis_base /bin/bash -c "echo 'Hello World!' > /data/blah.txt; ls -l /data"
+docker run --env EMIS_USER_ID=$(id -u) -v$EMIS_PROPERTY_DATA:/data test/emis_base /bin/bash -c "python -c 'import flask'; echo 'Hello World!' > /data/blah.txt; ls -l /data"
 cat $EMIS_PROPERTY_DATA/blah.txt
 rm $EMIS_PROPERTY_DATA/blah.txt
